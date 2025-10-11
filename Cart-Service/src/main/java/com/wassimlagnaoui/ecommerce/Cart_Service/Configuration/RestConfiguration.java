13542df597e1a14f61df4c22e561e136cf93aa2b
wl0182 <lagnaouiw@gmail.com>
2025-10-11 15:36:28 +0100

    Add RestConfiguration classes for various services with LoadBalanced RestTemplate

---
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Configuration/RestConfiguration.java
@@ -0,0 +1,16 @@
+package com.wassimlagnaoui.ecommerce.Cart_Service.Configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfiguration {
+    // Configuration class for RestTemplate bean can be added here if needed
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
