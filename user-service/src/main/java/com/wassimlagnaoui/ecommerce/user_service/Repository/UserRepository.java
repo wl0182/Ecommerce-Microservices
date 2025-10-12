b3812225af8f6d04b51635f613eda8a35fd9dc2a
wl0182 <lagnaouiw@gmail.com>
2025-10-12 11:39:34 +0100

    Add custom query method to UserRepository for case-insensitive name search

--- user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Repository/UserRepository.java
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Repository/UserRepository.java
@@ -1,15 +1,22 @@
 package com.wassimlagnaoui.ecommerce.user_service.Repository;
 
-import com.wassimlagnaoui.ecommerce.user_service.Model.User;
+import com.wassimlagnaoui.ecommerce.user_service.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM users u WHERE u.name ILIKE %?1%")
+    List<User> findByNameContainingIgnoreCase(String name);
+
+
 
 }
