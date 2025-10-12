3b72c143e1dc0b3abc82eb5455427dec810f5209
wl0182 <lagnaouiw@gmail.com>
2025-10-12 12:06:46 +0100

    Add registerUser method to UserService for user registration

--- user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Service/UserService.java
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Service/UserService.java
@@ -2,16 +2,28 @@ package com.wassimlagnaoui.ecommerce.user_service.Service;
 
 
 import com.wassimlagnaoui.ecommerce.user_service.DTO.CreateUserDTO;
+import com.wassimlagnaoui.ecommerce.user_service.DTO.RegisterUserDTO;
 import com.wassimlagnaoui.ecommerce.user_service.DTO.UserDetails;
+import com.wassimlagnaoui.ecommerce.user_service.Model.Address;
 import com.wassimlagnaoui.ecommerce.user_service.Model.User;
 import com.wassimlagnaoui.ecommerce.user_service.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    // inject PasswordEncoder
+    @Autowired
+    @Qualifier("PasswordEncoder")
+    private BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -36,5 +48,33 @@ public class UserService {
         User savedUser = userRepository.save(user);
         return new UserDetails(savedUser.getId(), savedUser.getEmail(), savedUser.getName(), savedUser.getPhoneNumber());
     }
+
+    // register user with email and password
+    @Transactional
+    public UserDetails registerUser(RegisterUserDTO registerUserDTO){
+        User user = new User();
+        Address address = new Address();
+
+        user.setEmail(registerUserDTO.getEmail());
+        user.setName(registerUserDTO.getName());
+        user.setPhoneNumber(registerUserDTO.getPhoneNumber());
+        user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
+
+        address.setStreet(registerUserDTO.getAddressStreet());
+        address.setCity(registerUserDTO.getAddressCity());
+        address.setZip(registerUserDTO.getAddressZip());
+        address.setCountry(registerUserDTO.getAddressCountry());
+        address.setDefault(true);
+        address.setUser(user);
+
+        user.setAddresses(List.of(address));
+
+        User savedUser = userRepository.save(user);
+
+        return new UserDetails(savedUser.getId(), savedUser.getEmail(), savedUser.getName(), savedUser.getPhoneNumber());
+
+    }
+
+
 
 }
