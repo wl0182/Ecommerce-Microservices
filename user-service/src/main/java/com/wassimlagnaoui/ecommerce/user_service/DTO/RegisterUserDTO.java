0fd0e5e2b68c1696c22364ef8df54761afc72f94
wl0182 <lagnaouiw@gmail.com>
2025-10-12 12:05:09 +0100

    Add RegisterUserDTO for user registration data transfer

---
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/DTO/RegisterUserDTO.java
@@ -0,0 +1,28 @@
+package com.wassimlagnaoui.ecommerce.user_service.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserDTO {
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    @NonNull
+    private String password;
+
+    private String phoneNumber;
+
+    private String addressStreet;
+    private String addressCity;
+    private String addressZip;
+    private String addressCountry;
+}
+
+
+//{ name, email, password }
