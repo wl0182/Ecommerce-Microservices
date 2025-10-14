afbd3f1a68d8f88a94bca7e703b7b119b66346ea
wl0182 <lagnaouiw@gmail.com>
2025-10-15 00:16:30 +0100

    Add ServiceURL class for defining service endpoints

---
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Util/ServiceURL.java
@@ -0,0 +1,6 @@
+package com.wassimlagnaoui.ecommerce.Cart_Service.Util;
+
+public class ServiceURL {
+    public static final String PRODUCT_SERVICE = "http://PRODUCT-SERVICE/products";
+    public static final String ORDER_SERVICE = "http://ORDER-SERVICE/orders";
+}
