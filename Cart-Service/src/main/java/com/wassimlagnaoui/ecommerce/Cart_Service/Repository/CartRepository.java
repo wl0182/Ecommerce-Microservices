1052c3b3ec4acc4a4bdf9486015c923a13d7a321
wl0182 <lagnaouiw@gmail.com>
2025-10-15 00:15:46 +0100

    Add custom query method to CartRepository for fetching cart by user ID

--- Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartRepository.java
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartRepository.java
@@ -2,8 +2,14 @@ package com.wassimlagnaoui.ecommerce.Cart_Service.Repository;
 
 import com.wassimlagnaoui.ecommerce.Cart_Service.Domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("SELECT c FROM Cart c WHERE c.userId = ?1")
+    Optional<Cart> findByUserId(Long userId);
 }
