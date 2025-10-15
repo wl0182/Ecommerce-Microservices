9a1e22cbacbc875e046e71b3056ab9ccb87d09bd
wl0182 <lagnaouiw@gmail.com>
2025-10-15 19:26:19 +0100

    Add custom queries to CartItemRepository for retrieving and deleting items by cart ID and product ID

--- Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartItemRepository.java
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartItemRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
@@ -16,4 +17,12 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findByCartId(Long cartId);
 
 
+
+
+
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.productId = ?2")
+    Optional<CartItem> findByCartIdAndProductId(Long id, Long productId);
+
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.id = ?1")
+    void deleteByCartId(Long id);
 }
