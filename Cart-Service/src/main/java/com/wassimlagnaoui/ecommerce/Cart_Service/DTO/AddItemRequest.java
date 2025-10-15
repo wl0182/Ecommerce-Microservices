ceac40a67d31d0c81e5db693b2e56385b3600136
wl0182 <lagnaouiw@gmail.com>
2025-10-15 08:52:02 +0100

    Add AddItemRequest DTO for adding items to the cart

---
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/DTO/AddItemRequest.java
@@ -0,0 +1,14 @@
+package com.wassimlagnaoui.ecommerce.Cart_Service.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddItemRequest {
+    // { productId, quantity }
+    private Long productId;
+    private Integer quantity;
+}
