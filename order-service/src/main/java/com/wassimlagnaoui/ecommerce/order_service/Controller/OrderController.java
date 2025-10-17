50a3f9e1bbd1ce8d5996801fe4a4868ad1bee0a5
wl0182 <lagnaouiw@gmail.com>
2025-10-17 17:06:03 +0100

    Refactor OrderController and OrderService to streamline order creation and response handling

--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Controller/OrderController.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Controller/OrderController.java
@@ -1,11 +1,7 @@
 package com.wassimlagnaoui.ecommerce.order_service.Controller;
 
 
-import com.wassimlagnaoui.ecommerce.order_service.DTO.CreateOrderDTO;
-import com.wassimlagnaoui.ecommerce.order_service.DTO.OrderDetails;
-import com.wassimlagnaoui.ecommerce.order_service.DTO.UserDetails;
 import com.wassimlagnaoui.ecommerce.order_service.Service.OrderService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,36 +13,6 @@ public class OrderController {
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
-
-    // get Order by id
-    @GetMapping("/{id}")
-    ResponseEntity<OrderDetails> getOrderById(@PathVariable Long id){
-        OrderDetails orderDetails = orderService.getOrderById(id);
-        if(orderDetails != null){
-            return ResponseEntity.ok(orderDetails);
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-
-    @PostMapping
-    ResponseEntity<OrderDetails> createOrder(@RequestBody CreateOrderDTO createOrderDTO){
-        OrderDetails orderDetails = orderService.createOrder(createOrderDTO);
-        return ResponseEntity.ok(orderDetails);
-    }
-
-    @GetMapping("/user/{userId}")
-    ResponseEntity<UserDetails> getUserDetails(@PathVariable Long userId){
-      UserDetails userDetails = orderService.getUserDetails(userId);
-        if(userDetails != null){
-            return ResponseEntity.ok(userDetails);
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-
-    }
-
 
 
 
--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Service/OrderService.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Service/OrderService.java
@@ -1,113 +1,73 @@
 package com.wassimlagnaoui.ecommerce.order_service.Service;
 
 import com.wassimlagnaoui.ecommerce.order_service.DTO.CreateOrderDTO;
-import com.wassimlagnaoui.ecommerce.order_service.DTO.OrderDetails;
-import com.wassimlagnaoui.ecommerce.order_service.DTO.OrderItemDTO;
-import com.wassimlagnaoui.ecommerce.order_service.DTO.UserDetails;
+import com.wassimlagnaoui.ecommerce.order_service.DTO.OrderCreatedResponse;
 import com.wassimlagnaoui.ecommerce.order_service.Entities.Order;
 import com.wassimlagnaoui.ecommerce.order_service.Entities.OrderItem;
+import com.wassimlagnaoui.ecommerce.order_service.Entities.OrderStatus;
 import com.wassimlagnaoui.ecommerce.order_service.Repository.OrderItemRepository;
 import com.wassimlagnaoui.ecommerce.order_service.Repository.OrderRepository;
-import com.wassimlagnaoui.ecommerce.order_service.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
-    private final ProductRepository productRepository;
+
 
 
     // adding RestTemplate to call other services
     @Autowired
-    private  RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
 
-
-
-
-    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, ProductRepository productRepository) {
+    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
         this.orderItemRepository = orderItemRepository;
         this.orderRepository = orderRepository;
-        this.productRepository = productRepository;
 
     }
 
-    // Get order by id with items
-    @Transactional(readOnly = true)
-    public OrderDetails getOrderById(Long id) {
-        return orderRepository.findById(id)
-                .map(order -> new OrderDetails(
-                        order.getOrderDate().toString(),
-                        order.getStatus(),
-                        order.getTotalAmount(),
-                        order.getUserId(),
-                        orderItemRepository.findOrderItemsByOrderId(order.getId())
-                ))
-                .orElse(null);
-    }
-
-
-    // create order
     @Transactional
-    public OrderDetails createOrder(CreateOrderDTO createOrderDTO) {
+    public OrderCreatedResponse createOrder(Long userId, CreateOrderDTO createOrderDTO){
         Order order = new Order();
-        order.setUserId(createOrderDTO.getUserId());
-        order.setStatus("PENDING");
-        order.setUserId(createOrderDTO.getUserId());
+        order.setUserId(userId);
+        order.setStatus(OrderStatus.PENDING.name());
         order.setOrderDate(LocalDateTime.now());
-        order.setTotalAmount(createOrderDTO.getTotalAmount());
 
-        List<OrderItem> orderItems = new ArrayList<>();
-
-        for (OrderItemDTO orderItemDTO : createOrderDTO.getItems()) {
-            OrderItem orderItem = new OrderItem();
-            orderItem.setProductId(orderItemDTO.getProductId());
-            orderItem.setQuantity(orderItemDTO.getQuantity());
-            orderItem.setPrice(productRepository.findById(orderItemDTO.getProductId())
-                    .map(product -> product.getPrice() * orderItemDTO.getQuantity())
-                    .orElse(0.0));
-            orderItem.setOrder(order);
-            orderItems.add(orderItem);
-        }
+        // retrieve items from createOrderDTO and set them to order
+        List<OrderItem> orderItems = createOrderDTO.getItems().stream()
+                .map(orderItemDTO -> {
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setProductId(orderItemDTO.getProductId());
+                    orderItem.setQuantity(orderItemDTO.getQuantity());
+                    orderItem.setPrice(orderItemDTO.getPrice());
+                    return orderItem;
+                }).collect(Collectors.toUnmodifiableList());
 
         order.setOrderItems(orderItems);
+
+        Double totalAmount = orderItems.stream().mapToDouble(value -> value.getPrice()*value.getQuantity()).sum();
+
         Order savedOrder = orderRepository.save(order);
-        orderItemRepository.saveAll(orderItems);
 
-        return new OrderDetails(
-                savedOrder.getOrderDate().toString(),
-                savedOrder.getStatus(),
-                savedOrder.getTotalAmount(),
-                savedOrder.getUserId(),
-                orderItems
-        );
-    }
-
-    // get User details
-    public UserDetails getUserDetails(Long userId) {
-        try {
-        UserDetails userDetails = restTemplate.getForObject("http://user-service/api/users/" + userId, UserDetails.class);
-        System.out.println("Retrieved user details: " + userDetails);
-        return userDetails;
-        }
-        catch (Exception e){
-            throw new RuntimeException("User service is down" + e.getMessage());
-        }
-
+        return OrderCreatedResponse.builder()
+                .id(savedOrder.getId())
+                .items(createOrderDTO.getItems())
+                .status(order.getStatus())
+                .totalAmount(totalAmount)
+                .userId(userId)
+                .createdAt(LocalDateTime.now().toString())
+                .build();
 
     }
 
 
-    }
+}
