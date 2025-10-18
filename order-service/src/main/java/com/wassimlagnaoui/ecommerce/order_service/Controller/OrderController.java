403afec6ad2d08c501bd7ed93f891d57de46ee91
wl0182 <lagnaouiw@gmail.com>
2025-10-18 22:23:23 +0100

    Add order creation, retrieval, cancellation, and status endpoints in OrderController

--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Controller/OrderController.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.wassimlagnaoui.ecommerce.order_service.Controller;
 
 
+import com.wassimlagnaoui.ecommerce.order_service.DTO.*;
 import com.wassimlagnaoui.ecommerce.order_service.Service.OrderService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -13,6 +17,46 @@ public class OrderController {
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
+
+
+
+    // create new Order
+    @PostMapping("/{userId}")
+    public ResponseEntity<OrderCreatedResponse> createOrder(@PathVariable("userId") Long userId,@RequestBody CreateOrderDTO createOrderDTO) {
+        OrderCreatedResponse orderDTO = orderService.createOrder(userId,createOrderDTO);
+        return ResponseEntity.ok(orderDTO);
+    }
+
+    // get order by id
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable("orderId") Long orderId) {
+        OrderDTO orderDTO = orderService.getOrderById(orderId);
+        return ResponseEntity.ok(orderDTO);
+    }
+
+    // get order by user id
+    public ResponseEntity<List<OrderDTO>> getOrdersByUserId(@PathVariable("userId") Long userId) {
+        List<OrderDTO> orderDTO = orderService.getOrdersByUserId(userId);
+        return ResponseEntity.ok(orderDTO);
+    }
+
+    // cancel order
+    @PutMapping("/cancel/{orderId}")
+    public ResponseEntity<OrderCancelled> cancelOrder(@PathVariable("orderId") Long orderId) {
+        OrderCancelled response = orderService.cancelOrder(orderId);
+        return ResponseEntity.ok(response);
+    }
+
+
+    // get order status
+    @GetMapping("/status/{orderId}")
+    public ResponseEntity<OrderStatusDTO> getOrderStatus(@PathVariable("orderId") Long orderId) {
+        OrderStatusDTO status = orderService.getOrderStatus(orderId);
+        return ResponseEntity.ok(status);
+    }
+
+
+
 
 
 
