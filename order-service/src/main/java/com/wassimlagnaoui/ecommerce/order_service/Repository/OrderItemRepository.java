de477b61e9316466928a1f52d462398ce45b5e92
wl0182 <lagnaouiw@gmail.com>
2025-10-18 14:30:19 +0100

    Add method to find OrderItems by order ID in OrderItemRepository

--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Repository/OrderItemRepository.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Repository/OrderItemRepository.java
@@ -11,4 +11,7 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     @Query("SELECT oi FROM OrderItem oi WHERE oi.order.id = :orderId")
     List<OrderItem> findOrderItemsByOrderId(Long orderId);
+
+
+    List<OrderItem> findByOrderId(Long orderId);
 }
