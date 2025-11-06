088b896ef6672f5c2d60619938cb7796d502985f
wl0182 <lagnaouiw@gmail.com>
2025-11-06 12:14:17 +0100

    Add custom exception handler for OrderNotFoundForUser with detailed response

--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Exception/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Exception/GlobalExceptionHandler.java
@@ -13,6 +13,17 @@ public class GlobalExceptionHandler {
         response.put("error", ex.getMessage());
         response.put("status", "404");
         response.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        response.put("info", "Order Not Found, please check the order ID and try again.");
+        return response;
+    }
+
+    @ExceptionHandler(OrderNotFoundForUser.class)
+    public HashMap<String,String> handleOrderNotFoundForUserException(OrderNotFoundForUser ex) {
+        HashMap<String,String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        response.put("status", "404");
+        response.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        response.put("info", "No orders found for the specified user ID, please verify and try again.");
         return response;
     }
 
