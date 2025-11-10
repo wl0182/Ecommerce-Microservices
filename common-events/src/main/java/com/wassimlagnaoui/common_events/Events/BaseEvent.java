694de837e8bfbee10a5bf0ed5986b5d703c5e5a7
wl0182 <lagnaouiw@gmail.com>
2025-11-10 23:09:34 +0100

    Remove Builder Annotation as the baseEvent is inherited

--- common-events/src/main/java/com/wassimlagnaoui/common_events/Events/BaseEvent.java
+++ common-events/src/main/java/com/wassimlagnaoui/common_events/Events/BaseEvent.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.Instant;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BaseEvent {
