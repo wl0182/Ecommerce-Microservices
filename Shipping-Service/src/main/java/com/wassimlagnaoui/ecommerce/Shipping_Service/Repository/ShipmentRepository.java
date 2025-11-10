a2968f2f1b8fdba78cf615f97562d3ac0a18c75d
wl0182 <lagnaouiw@gmail.com>
2025-11-10 15:50:43 +0100

    Added FindByOrderID to repo

--- Shipping-Service/src/main/java/com/wassimlagnaoui/ecommerce/Shipping_Service/Repository/ShipmentRepository.java
+++ Shipping-Service/src/main/java/com/wassimlagnaoui/ecommerce/Shipping_Service/Repository/ShipmentRepository.java
@@ -2,8 +2,17 @@ package com.wassimlagnaoui.ecommerce.Shipping_Service.Repository;
 
 import com.wassimlagnaoui.ecommerce.Shipping_Service.Domain.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ShipmentRepository extends JpaRepository<Shipment,Long> {
+
+
+
+    // get Shipment by orderId
+    @Query("SELECT s FROM Shipment s WHERE s.orderId = ?1")
+    Optional<Shipment> findByOrderId(Long orderId);
 }
