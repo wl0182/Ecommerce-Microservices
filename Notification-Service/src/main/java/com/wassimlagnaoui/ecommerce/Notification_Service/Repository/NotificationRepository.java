fe64f27334c8cbe96bb267409513b0840b623e99
wl0182 <lagnaouiw@gmail.com>
2025-11-10 23:12:32 +0100

    added findByUserId to my NotificationRepository

--- Notification-Service/src/main/java/com/wassimlagnaoui/ecommerce/Notification_Service/Repository/NotificationRepository.java
+++ Notification-Service/src/main/java/com/wassimlagnaoui/ecommerce/Notification_Service/Repository/NotificationRepository.java
@@ -2,9 +2,14 @@ package com.wassimlagnaoui.ecommerce.Notification_Service.Repository;
 
 import com.wassimlagnaoui.ecommerce.Notification_Service.Domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 
+    @Query(nativeQuery = true, value = "SELECT * FROM notifications n WHERE n.user_id = ?1")
+    List<Notification> findByUserId(Long userId);
 }
