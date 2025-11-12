7e055a15788d3e76cdd8356fdb2067e49aa851ba
wl0182 <lagnaouiw@gmail.com>
2025-11-12 11:34:09 +0100

    Add MessageDTO class for message handling

---
+++ product-service/src/main/java/com/wassimlagnaoui/ecommerce/product_service/DTO/MessageDTO.java
@@ -0,0 +1,15 @@
+package com.wassimlagnaoui.ecommerce.product_service.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageDTO {
+    private String message;
+
+}
