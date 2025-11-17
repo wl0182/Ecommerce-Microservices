a4a1715f6bf25414d83ac3d6c7f5f799a0ca2eb9
wl0182 <lagnaouiw@gmail.com>
2025-11-18 00:05:07 +0100

    Refactor Kafka topic names to use constants from KafkaTopics class

--- Payment-Service/src/main/java/com/wassimlagnaoui/ecommerce/Payment_Service/Configuration/KafkaConfiguration.java
+++ Payment-Service/src/main/java/com/wassimlagnaoui/ecommerce/Payment_Service/Configuration/KafkaConfiguration.java
@@ -1,5 +1,6 @@
 package com.wassimlagnaoui.ecommerce.Payment_Service.Configuration;
 
+import com.wassimlagnaoui.common_events.KafkaTopics;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,11 @@ public class KafkaConfiguration {
 
     @Bean
     public NewTopic paymentProcessedTopic() {
-        return new NewTopic("payment-processed", 2, (short) 1); // 3 partitions, 1 replica
+        return new NewTopic(KafkaTopics.PAYMENT_PROCESSED, 2, (short) 1); // 3 partitions, 1 replica
     }
     @Bean
     public NewTopic paymentRefundedTopic() {
-        return new NewTopic("payment-refunded", 1, (short) 1); // 3 partitions, 1 replica
+        return new NewTopic(KafkaTopics.PAYMENT_REFUNDED, 1, (short) 1); // 3 partitions, 1 replica
     }
 
     @Bean
