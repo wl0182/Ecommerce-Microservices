1d5bc39bffa6cd1e0557d3df230465981f6d99ef
wl0182 <lagnaouiw@gmail.com>
2025-11-18 23:16:16 +0100

    Refactor Kafka topic configuration to use constants from KafkaTopics and add orderPaid topic

--- order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Configuration/KafkaConfiguration.java
+++ order-service/src/main/java/com/wassimlagnaoui/ecommerce/order_service/Configuration/KafkaConfiguration.java
@@ -1,6 +1,7 @@
 package com.wassimlagnaoui.ecommerce.order_service.Configuration;
 
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.wassimlagnaoui.common_events.KafkaTopics;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -18,13 +19,19 @@ public class KafkaConfiguration {
     // New Topic Bean
     @Bean
     public NewTopic orderCreatedTopic(){
-        return new NewTopic("order-created",3,(short)1); // 3 partitions, 1 replica
+        return new NewTopic(KafkaTopics.ORDER_CREATED,3,(short)1); // 3 partitions, 1 replica
     }
 
 
     @Bean
     public NewTopic orderCancelledTopic(){
-        return new NewTopic("order-cancelled",2,(short)1); // 3 partitions, 1 replica
+        return new NewTopic(KafkaTopics.ORDER_CANCELLED,2,(short)1); // 2 partitions, 1 replica
+    }
+
+    // order paid topic
+    @Bean
+    public NewTopic orderPaidTopic(){
+        return new NewTopic(KafkaTopics.ORDER_PAID,2,(short)1); // 2 partitions, 1 replica
     }
 
 
