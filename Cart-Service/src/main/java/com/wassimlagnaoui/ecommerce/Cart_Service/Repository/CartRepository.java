56977b1e9f7789f3ae17afd45f6e172b7fd32f7b
wl0182 <lagnaouiw@gmail.com>
2025-12-09 19:28:14 +0100

    Add query method to CartRepository for fetching carts updated before a specific time

--- Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartRepository.java
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Repository/CartRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,8 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.userId = ?1")
     Optional<Cart> findByUserId(Long userId);
+
+
+    @Query("SELECT c FROM Cart c WHERE c.updatedAt < ?1")
+    List<Cart> findByUpdatedAtBefore(LocalDateTime cutoffTime);
 }
