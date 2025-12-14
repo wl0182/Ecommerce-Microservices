e72519791ef03ebd86cbabed2d7aaf7f506c5391
wl0182 <lagnaouiw@gmail.com>
2025-12-14 17:06:58 +0100

    Add event handlers for payment processed and payment failed notifications

--- Notification-Service/src/main/java/com/wassimlagnaoui/ecommerce/Notification_Service/Service/NotificationEventListener.java
+++ Notification-Service/src/main/java/com/wassimlagnaoui/ecommerce/Notification_Service/Service/NotificationEventListener.java
@@ -2,6 +2,8 @@ package com.wassimlagnaoui.ecommerce.Notification_Service.Service;
 
 import com.wassimlagnaoui.common_events.DummyEvent;
 import com.wassimlagnaoui.common_events.Events.OrderService.OrderCreateEvent;
+import com.wassimlagnaoui.common_events.Events.PaymentService.PaymentFailed;
+import com.wassimlagnaoui.common_events.Events.PaymentService.PaymentProcessed;
 import com.wassimlagnaoui.common_events.Events.UserService.UserRegisteredEvent;
 import com.wassimlagnaoui.common_events.KafkaGroupIds;
 import com.wassimlagnaoui.common_events.KafkaTopics;
@@ -54,6 +56,33 @@ public class NotificationEventListener {
         log.info("Processed OrderCreateEvent for order: " + event.getOrderId());
         log.info("Email Sent to User for Order Confirmation");
     }
+
+    // handle Payment Processed Event to send payment confirmation email
+    @KafkaListener(topics = KafkaTopics.PAYMENT_PROCESSED,groupId = KafkaGroupIds.NOTIFICATION_SERVICE_GROUP)
+    public void handlePaymentProcessedEvent(PaymentProcessed paymentProcessed) {
+        // Implementation goes here
+        String subject = "Payment Confirmation - Order #" + paymentProcessed.getOrderId();
+        String body = templateService.renderPaymentProcessedTemplate(paymentProcessed);
+        emailService.sendEmail("lagnaouiw@gmail.com",subject,body);
+        System.out.println("Processed PaymentProcessed Event for order: " + paymentProcessed.getOrderId());
+        log.info("Processed PaymentProcessed Event for order: " + paymentProcessed.getOrderId());
+        log.info("Email Sent to User for Payment Confirmation");
+    }
+
+    // handle payment failed event to send payment failure email
+    @KafkaListener(topics = KafkaTopics.PAYMENT_FAILED,groupId = KafkaGroupIds.NOTIFICATION_SERVICE_GROUP)
+    public void handlePaymentFailedEvent(PaymentFailed paymentFailed) {
+        String subject = "Payment Failed - Order #" + paymentFailed.getOrderId();
+        String body = templateService.renderPaymentFailedTemplate(paymentFailed);
+        emailService.sendEmail("lagnaouiw@gmail.com",subject,body);
+        System.out.println("Processed PaymentFailed Event for order: " + paymentFailed.getOrderId());
+        log.info("Processed PaymentFailed Event for order: " + paymentFailed.getOrderId());
+        log.info("Email Sent to User for Payment Failure");
+    }
+
+
+
+
 
 
 
