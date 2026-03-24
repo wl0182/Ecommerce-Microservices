da36e10df385e22a8850318a0b53faa0a6a8d126
wl0182 <lagnaouiw@gmail.com>
2026-03-24 17:28:49 +0300

    Creating InventoryDTO class to consume Product-Service get Inventory API

---
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/DTO/RestDTOs/InventoryDTO.java
@@ -0,0 +1,17 @@
+package com.wassimlagnaoui.ecommerce.Cart_Service.DTO.RestDTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryDTO {
+    private Long productId;
+    private String productName;
+    private String sku;
+    private Integer stockInventory;
+}
