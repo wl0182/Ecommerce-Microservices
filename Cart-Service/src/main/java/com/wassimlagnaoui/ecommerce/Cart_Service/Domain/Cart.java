238b310fc2e5ea0653e03cbb099a44a9c4a8c087
wl0182 <lagnaouiw@gmail.com>
2026-03-27 15:38:53 +0300

    Initializing cartItem list in the entity to avoid nullpointeException in addItemToCart Method

--- Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Domain/Cart.java
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Domain/Cart.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -24,7 +25,7 @@ public class Cart {
     private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true ,fetch = FetchType.LAZY)
-    private List<CartItem> cartItems;
+    private List<CartItem> cartItems = new ArrayList<>();
 
 
 }
