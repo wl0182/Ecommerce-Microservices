b2d3ee5a02775cab2232230a029796e36ecd8af2
wl0182 <lagnaouiw@gmail.com>
2026-03-31 15:09:17 +0300

    Refactor RestConfiguration to use JDK HttpClient for RestClient.Builder; set connection and read timeouts for improved resilience

--- Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Configuration/RestConfiguration.java
+++ Cart-Service/src/main/java/com/wassimlagnaoui/ecommerce/Cart_Service/Configuration/RestConfiguration.java
@@ -1,15 +1,12 @@
 package com.wassimlagnaoui.ecommerce.Cart_Service.Configuration;
 
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestConfiguration {
@@ -20,10 +17,20 @@ public class RestConfiguration {
         return new RestTemplate();
     }
 
+
     @Bean
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+
+        var httpClient = java.net.http.HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        var requestFactory = new org.springframework.http.client.JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory);
     }
 
     @Bean
