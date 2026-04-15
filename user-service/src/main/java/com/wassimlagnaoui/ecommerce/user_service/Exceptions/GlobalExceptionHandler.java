9d022b181027dec4552315773071326e7ed57c84
wl0182 <lagnaouiw@gmail.com>
2026-04-15 15:43:38 +0300

    Refactor application configuration, add EventStatus enum, and enhance global exception handling for validation errors

--- user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Exceptions/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.wassimlagnaoui.ecommerce.user_service.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -33,4 +35,23 @@ public class GlobalExceptionHandler {
         response.put("status", "401");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
+
+    // MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("errors", fieldErrors);
+        response.put("timestamp", System.currentTimeMillis());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
 }
---
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Model/EventStatus.java
@@ -0,0 +1,7 @@
+package com.wassimlagnaoui.ecommerce.user_service.Model;
+
+public enum EventStatus {
+    PENDING,
+    PROCESSED,
+    FAILED
+}
