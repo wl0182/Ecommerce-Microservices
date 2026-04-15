e8db4272a10ff74d89e016380e50d75db08ff004
wl0182 <lagnaouiw@gmail.com>
2026-04-15 15:42:42 +0300

    Refactor Address entity and repository to enhance data access methods

--- user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Model/Address.java
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Model/Address.java
@@ -5,11 +5,10 @@ import jakarta.persistence.*;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Repository/AddressRepository.java
+++ user-service/src/main/java/com/wassimlagnaoui/ecommerce/user_service/Repository/AddressRepository.java
@@ -3,8 +3,17 @@ package com.wassimlagnaoui.ecommerce.user_service.Repository;
 
 import com.wassimlagnaoui.ecommerce.user_service.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    // find all addresses by user id
+    List<Address> findByUserId(Long userId);
+    // find default address by user id
+    @Query("SELECT a FROM Address a WHERE a.user.id = ?1 AND a.isDefault = true")
+    Address findByUserIdAndIsDefaultTrue(Long userId);
 }
