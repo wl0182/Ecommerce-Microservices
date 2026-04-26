11d5be3ed9d3e5fb4d1af62fe714efca98463f25
wl0182 <lagnaouiw@gmail.com>
2026-04-26 12:05:38 +0300

    Add serialization exception handling in PaymentService and GlobalExceptionHandler

--- Payment-Service/src/main/java/com/wassimlagnaoui/ecommerce/Payment_Service/Exception/GlobalExceptionHandler.java
+++ Payment-Service/src/main/java/com/wassimlagnaoui/ecommerce/Payment_Service/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.wassimlagnaoui.ecommerce.Payment_Service.Exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.http.protocol.HTTP;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,6 +43,28 @@ public class GlobalExceptionHandler {
         response.put("validationErrors", ex.getBindingResult().getFieldErrors().stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .toList());
+        return response;
+    }
+
+    // Json parse exception
+    @ExceptionHandler(JsonProcessingException.class)
+    public HashMap<String, String> handleJsonProcessingException(JsonProcessingException ex) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("error", "Failed to process JSON data.");
+        response.put("status", HttpStatus.BAD_REQUEST.toString());
+        response.put("type", "Bad Request");
+        response.put("solution", "Please check the JSON data for syntax errors and try again.");
+        return response;
+    }
+
+    // handle SerializationException
+    @ExceptionHandler(SerialiazationException.class)
+    public HashMap<String, String> handleSerializationException(SerialiazationException ex) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("error", "Failed to serialize data.");
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.toString());
+        response.put("type", "Internal Server Error");
+        response.put("solution", "Please check the server logs for more details and try again.");
         return response;
     }
 }
---
+++ Payment-Service/src/main/java/com/wassimlagnaoui/ecommerce/Payment_Service/Exception/SerialiazationException.java
@@ -0,0 +1,7 @@
+package com.wassimlagnaoui.ecommerce.Payment_Service.Exception;
+
+public class SerialiazationException extends RuntimeException {
+    public SerialiazationException(String message) {
+        super(message);
+    }
+}
